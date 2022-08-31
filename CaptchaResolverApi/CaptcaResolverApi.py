98907ec9a41c3b07619a9646fcc4d813d3bc3068
Fahad-ishfaq <sardarfahadashfaq@gmail.com>
2022-08-31 13:14:13 +0500

    recaptcha api and requirements

--- CaptchaResolverApi/CaptcaResolverApi.py
+++ CaptchaResolverApi/CaptcaResolverApi.py
@@ -1,5 +1,15 @@
 from twocaptcha import TwoCaptcha
 import os
 solver = TwoCaptcha(os.getenv("CAPTCHA_API"))
-result = solver.normal('images/1.jpeg', caseSensitive = 1)
-print(result);
+type = "normal"
+
+if (type == "normal"):
+    print("Solving normal captcha")
+    result = solver.normal('images/1.jpeg', caseSensitive = 1)
+    print(result);
+else:
+    print("Solving Recaptcha2")
+    # =============== ReCaptcha 2 =========================== #
+    recaptcha2 = solver.recaptcha(sitekey='6Lc7cyAaAAAAAGUwpzByQGxfnK1A-BOPfOzuQCA2', 
+                                    url='https://demo.whoisfreaks.com/live')
+    print(recaptcha2)
